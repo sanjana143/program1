bef2ceb8082684264d127284ead557c28e0602e3
sanjana143 <56549675+sanjana143@users.noreply.github.com>
2019-12-28 14:06:44 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Sum
+{
+public static void main(String args[])
+{
+Scanner sc=new  Scanner(System.in);
+Systemcd Descd.out.println("Enter a value");
+int a=sc.nextInt();
+System.out.println("Enter b value");
+int b=sc.nextInt();
+System.out.println(a+b);
+}
+}
